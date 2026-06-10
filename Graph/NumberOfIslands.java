f0a6997bd46e4d575b3eb27554c2841582ac62a7
Dev Shrivastava <devshrivastava621@gmail.com>
2026-06-10 21:19:00 +0530

    Solved Number Of Islands using DFS

--- Graph/NumberOfIslands.java
+++ Graph/NumberOfIslands.java
@@ -1,49 +1,38 @@
 public class NumberOfIslands {
+    private void dfs(char[][] grid,int i,int j)
+    {
+        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j] == '0')
+        {
+            return;
+        }
+        grid[i][j] = '0';
+
+        dfs(grid, i+1, j);
+        dfs(grid, i-1, j);
+        dfs(grid,i,j+1);
+        dfs(grid, i, j-1);
+        return;
+    }
 
     public int numIslands(char[][] grid) {
-
-        if (grid == null || grid.length == 0) {
+        if(grid == null || grid.length == 0 || grid[0].length == 0)
+        {
             return 0;
         }
-
-        int rows = grid.length;
-        int cols = grid[0].length;
-        int islands = 0;
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-
-                // Agar land mila
-                if (grid[i][j] == '1') {
-                    islands++;          // Naya island
-                    dfs(grid, i, j);    // Pure island ko mark kar do
+        int count = 0;
+        for(int i = 0;i<grid.length;i++)
+        {
+            for(int j=0;j<grid[0].length;j++)
+            {
+                if(grid[i][j] == '1')
+                {
+                    dfs(grid, i , j);
+                    count++;
                 }
             }
         }
+        return count;
 
-        return islands;
-    }
-
-    private void dfs(char[][] grid, int row, int col) {
-
-        int rows = grid.length;
-        int cols = grid[0].length;
-
-        // Boundary check + water check
-        if (row < 0 || row >= rows ||
-            col < 0 || col >= cols ||
-            grid[row][col] == '0') {
-            return;
-        }
-
-        // Visited mark kar diya
-        grid[row][col] = '0';
-
-        // 4 directions
-        dfs(grid, row + 1, col); // Down
-        dfs(grid, row - 1, col); // Up
-        dfs(grid, row, col + 1); // Right
-        dfs(grid, row, col - 1); // Left
     }
     public static void main(String[] args)
     {
