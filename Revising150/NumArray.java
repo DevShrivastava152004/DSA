3b01140a319788075c9af8810f65923dba2a003b
Dev Shrivastava <devshrivastava621@gmail.com>
2026-06-20 23:47:49 +0530

    Solved Num Array

---
+++ Revising150/NumArray.java
@@ -0,0 +1,24 @@
+public class NumArray {
+    private int[] prefix;
+
+    public NumArray(int[] nums) {
+         prefix = new int[nums.length + 1];
+
+        for (int i = 0; i < nums.length; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+    }
+    
+    public int sumRange(int left, int right) {
+         return prefix[right + 1] - prefix[left];
+        
+    }
+    public static void main(String[] args)
+    {
+        int[] nums = {-2, 0, 3, -5, 2, -1};
+        NumArray numArray = new NumArray(nums);
+        System.out.println(numArray.sumRange(0, 2)); // Output: 1
+        System.out.println(numArray.sumRange(2, 5)); // Output: -1
+        System.out.println(numArray.sumRange(0, 5)); // Output: -3
+    }
+} // the time complexity of this code is O(n) for the constructor and O(1) for the sumRange method. The space complexity is O(n) due to the prefix array.
